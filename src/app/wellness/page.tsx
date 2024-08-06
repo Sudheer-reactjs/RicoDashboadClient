d78cfc2dcfcb67f759c228b911d90b4ecf10fe15
Sudheer-reactjs <info@auspicioussoft.com>
2024-08-06 11:34:50 +0530

    New changes

--- src/app/wellness/page.tsx
+++ src/app/wellness/page.tsx
@@ -1,9 +1,44 @@
+"use client";
+import { useState } from 'react';
+const Page = () => {
+    const [activeTab, setActiveTab] = useState('Previous Appointments');
 
-const page = () => {
-    return(
-        <>
-           Wellness
-        </>
-    );
+    const renderTabContent = () => {
+      switch (activeTab) {
+        case 'Previous Appointments':
+          return <div>Content for Previous Appointments</div>;
+        case 'Upcoming Appointments':
+          return <div>Content for Upcoming Appointments</div>;
+        case 'Videos':
+          return <div>Content for Videos</div>;
+        case 'Attachments':
+          return <div>Content for Attachments</div>;
+        default:
+          return null;
+      }
+    };
+  return (
+    <>
+      <h1 className="font-antic text-[#283C63] text-[30px] leading-[1.2em] mb-[25px] lg:text-[40px] lg:mb-[50px]">
+        wellness portal<span>sdfsdfs</span>
+      </h1>
+      <div>
+      <div className="tabs">
+        {['Previous Appointments', 'Upcoming Appointments', 'Videos', 'Attachments'].map((tab) => (
+          <button
+            key={tab}
+            className={`tab-button ${activeTab === tab ? 'active' : ''}`}
+            onClick={() => setActiveTab(tab)}
+          >
+            {tab}
+          </button>
+        ))}
+      </div>
+      <div className="tab-content">
+        {renderTabContent()}
+      </div>
+    </div> 
+    </>
+  );
 };
-export default page
+export default Page;
--- src/components/DashboardAssignment.tsx
+++ src/components/DashboardAssignment.tsx
@@ -1,108 +1,120 @@
 "use client";
-import { useState, CSSProperties } from 'react';
-import Link from 'next/link';
-import { NextIcon, PervIcon, ViewIcon } from '@/utils/svgicon';
+import React, { ReactNode, useState, CSSProperties } from "react";
+import Link from "next/link";
+import ReactPaginate from "react-paginate";
+import Image from "next/image";
+import PervIcon from "../assets/images/pervicon.png";
+import NextIcon from "../assets/images/nexticon.png";
+import { ViewIcon, ButtonArrow } from "@/utils/svgicon";
+import Therapist1 from "../assets/images/therapist1.jpg";
+import Therapist2 from "../assets/images/therapist2.jpg";
+import Therapist3 from "../assets/images/therapist3.jpg";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+
+const Modal = ({ isOpen, onClose, children }: ModalProps) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+      <div className="bg-white w-[94%] max-w-[1200px] shadow-lg relative max-h-[90vh] overflo-custom py-[25px] px-[15px] lg:p-[40px]">
+        <button
+          onClick={onClose}
+          className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
+        >
+          &#x2715;
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
 
 const DashboardAssignment = () => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [renewPopupOpen, setRenewPopupOpen] = useState(false);
+  const [teamPopupOpen, setTeamPopupOpen] = useState(false);
+
+  const therapists = [
+    { id: 1, name: "Therapist One", imageUrl: Therapist1 },
+    { id: 2, name: "Therapist Two", imageUrl: Therapist2 },
+    { id: 3, name: "Therapist Three", imageUrl: Therapist3 },
+    { id: 4, name: "Therapist Three", imageUrl: Therapist3 },
+    // Add more therapists here
+  ];
+
   const data = [
-    { id: 1, apptDate: '26 July 2023', renewalDate: '04 Jan 2025', chatWithClinician: 'Start Chat', videoChat: 'Start Video Call', billingAmount: '$25.00',  },
-    { id: 2, apptDate: '26 July 2023', renewalDate: '04 Jan 2022', chatWithClinician: 'Renew Subscription', videoChat: 'Renew Subscription', billingAmount: '$25.00',  },
-    { id: 3, apptDate: '26 July 2023', renewalDate: '04 Jan 2025', chatWithClinician: 'Not Available', videoChat: 'Wait for approval', billingAmount: '$25.00', },
+    {
+      id: 1,
+      apptDate: "26 July 2023",
+      renewalDate: "04 Jan 2025",
+      chatWithClinician: "Start Chat",
+      videoChat: "Start Video Call",
+      billingAmount: "$25.00",
+    },
+    {
+      id: 2,
+      apptDate: "26 July 2023",
+      renewalDate: "04 Jan 2022",
+      chatWithClinician: "Renew Subscription",
+      videoChat: "Renew Subscription",
+      billingAmount: "$25.00",
+    },
+    {
+      id: 3,
+      apptDate: "26 July 2023",
+      renewalDate: "04 Jan 2025",
+      chatWithClinician: "Not Available",
+      videoChat: "Wait for approval",
+      billingAmount: "$25.00",
+    },
     // Add more data as needed
   ];
-//paginationStart
-  const [currentPage, setCurrentPage] = useState(0);  
-  const itemsPerPage = 10;
-  const totalPages = Math.ceil(data.length / itemsPerPage);
 
-  const handleNextPage = () => {
-    if (currentPage < totalPages - 1) {
-      setCurrentPage(currentPage + 1);
-    }
+  // ReactPaginate
+  const rowsPerPage = 10;
+  const indexOfLastRow = (currentPage + 1) * rowsPerPage;
+  const indexOfFirstRow = indexOfLastRow - rowsPerPage;
+  const currentRows = data.slice(indexOfFirstRow, indexOfLastRow);
+  const handlePageClick = (selectedItem: { selected: number }) => {
+    setCurrentPage(selectedItem.selected);
   };
-
-  const handlePrevPage = () => {
-    if (currentPage > 0) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-  };
-
-  const currentData = data.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage);
-
-  const renderPageNumbers = () => {
-    const pageNumbers = [];
-    const maxPagesToShow = 5; // Number of pages to display before and after the current page
-  
-    // Adjust start and end pages based on total pages
-    const startPage = Math.max(0, currentPage - Math.floor(maxPagesToShow / 2));
-    const endPage = Math.min(totalPages - 1, startPage + maxPagesToShow - 1);
-  
-    // Display page numbers in the visible range
-    for (let i = startPage; i <= endPage; i++) {
-      pageNumbers.push(
-        <button
-          key={i}
-          onClick={() => handlePageChange(i)}
-          style={{ background: currentPage === i ? '#26395E' : '', color: currentPage === i ? '#fff' : '' }}
-        >
-          {i + 1}
-        </button>
-      );
-    }
-  
-    // Display the last page
-    if (endPage < totalPages - 1) {
-      if (endPage < totalPages - 2) {
-        pageNumbers.push(<span key="ellipsis-end">...</span>);
-      }
-      pageNumbers.push(
-        <button key="end" onClick={() => handlePageChange(totalPages - 1)} disabled={currentPage === totalPages - 1}>
-          {totalPages}
-        </button>
-      );
-    } 
-
-    return pageNumbers;
-  };
-  
-  
-  //paginationEnd
 
   function isPastDate(dateString: string) {
-        const date = new Date(dateString);
-        const today = new Date();
-        return date < today;
-    }
+    const date = new Date(dateString);
+    const today = new Date();
+    return date < today;
+  }
 
   const getStyle = (text: string): CSSProperties => {
     let style: CSSProperties = {
-      padding: '2px 10px',
-      borderRadius: '20px',
-      display: 'inline-block',
-      fontSize: '10px',
+      padding: "2px 10px",
+      borderRadius: "20px",
+      display: "inline-block",
+      fontSize: "10px",
     };
 
     switch (text) {
-      case 'Renew Subscription':
-        style.backgroundColor = '#FFFCEC';
-        style.color = '#FFA234';
+      case "Renew Subscription":
+        style.backgroundColor = "#FFFCEC";
+        style.color = "#FFA234";
         break;
-      case 'Start Chat':
-      case 'Start Video Call':
-        style.backgroundColor = '#CBFFB2';
-        style.color = '#42A803';
+      case "Start Chat":
+      case "Start Video Call":
+        style.backgroundColor = "#CBFFB2";
+        style.color = "#42A803";
         break;
-      case 'Wait for approval':
-        style.backgroundColor = '#FFFCB2';
-        style.color = '#A85C03';
+      case "Wait for approval":
+        style.backgroundColor = "#FFFCB2";
+        style.color = "#A85C03";
         break;
-      case 'Not Available':
-        style.backgroundColor = '#FFBBCD';
-        style.color = '#bb2b51';
+      case "Not Available":
+        style.backgroundColor = "#FFBBCD";
+        style.color = "#bb2b51";
         break;
       default:
         break;
@@ -111,74 +123,254 @@ const DashboardAssignment = () => {
     return style;
   };
 
- 
+  const handleRenewClick = () => {
+    setRenewPopupOpen(true);
+  };
+  const renewClosePopup = () => {
+    setRenewPopupOpen(false);
+  };
+  const handleViewTeam = () => {
+    setTeamPopupOpen(true);
+  };
+  const handleCloseTeam = () => {
+    setTeamPopupOpen(false);
+  };
 
   return (
     <>
-    <h3 className='mb-[20px]'>Assignment details</h3>
-    <div className='table-common'>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Appt Date</th>
-            <th>Renewal Date</th>
-            <th>Chat With Clinician</th>
-            <th>Video Chat</th>
-            <th>Billing Amount</th>
-            <th>Care Team</th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentData.map((item) => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.apptDate}</td>
-              <td>
-                {isPastDate(item.renewalDate) ? (
-                  <Link href="/renewplane" style={getStyle('Renew Subscription')}>Renew Subscription</Link>
-                ) : (
-                  item.renewalDate
-                )}
-              </td>
-              <td>
-                {item.chatWithClinician === 'Start Chat' ? (
-                  <Link href="/chat" style={getStyle('Start Chat')}>{item.chatWithClinician}</Link>
-                ) : isPastDate(item.renewalDate) || item.chatWithClinician === 'Renew Subscription' ? (
-                  <Link href="/renewplane" style={getStyle('Renew Subscription')}>{item.chatWithClinician}</Link>
-                ) : (
-                  <span style={getStyle(item.chatWithClinician)}>{item.chatWithClinician}</span>
-                )}
-              </td>
-              <td>
-                {item.videoChat === 'Start Video Call' ? (
-                  <Link href="/video-call" style={getStyle('Start Video Call')}>{item.videoChat}</Link>
-                ) : isPastDate(item.renewalDate) || item.videoChat === 'Renew Subscription' ? (
-                  <Link href="/renewplane" style={getStyle('Renew Subscription')}>{item.videoChat}</Link>
-                ) : (
-                  <span style={getStyle(item.videoChat)}>{item.videoChat}</span>
-                )}
-              </td>
-              <td>{item.billingAmount}</td> 
-              <td>
-                <Link href="/care-team"><ViewIcon /></Link>
-              </td>
+      <h3 className="mb-[20px]">Assignment details</h3>
+      <div className="table-common">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Appt Date</th>
+              <th>Renewal Date</th>
+              <th>Chat With Clinician</th>
+              <th>Video Chat</th>
+              <th>Billing Amount</th>
+              <th>Care Team</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className='pagination-custom-outer'>
-      <div className='pagination-custom'>
-        <button onClick={handlePrevPage} disabled={currentPage === 0}>
-         <PervIcon />
-        </button>
-        {renderPageNumbers()}
-        <button onClick={handleNextPage} disabled={currentPage >= totalPages - 1}>
-          <NextIcon />
-        </button>
+          </thead>
+          <tbody>
+            {currentRows.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.apptDate}</td>
+                <td>
+                  {isPastDate(item.renewalDate) ? (
+                    <span
+                      style={getStyle("Renew Subscription")}
+                      onClick={handleRenewClick}
+                    >
+                      Renew Subscription
+                    </span>
+                  ) : (
+                    item.renewalDate
+                  )}
+                </td>
+                <td>
+                  {item.chatWithClinician === "Start Chat" ? (
+                    <span style={getStyle("Start Chat")}>
+                      {item.chatWithClinician}
+                    </span>
+                  ) : isPastDate(item.renewalDate) ||
+                    item.chatWithClinician === "Renew Subscription" ? (
+                    <span
+                      style={getStyle("Renew Subscription")}
+                      onClick={handleRenewClick}
+                    >
+                      Renew Subscription
+                    </span>
+                  ) : (
+                    <span style={getStyle(item.chatWithClinician)}>
+                      {item.chatWithClinician}
+                    </span>
+                  )}
+                </td>
+                <td>
+                  {item.videoChat === "Start Video Call" ? (
+                    <span
+                      style={getStyle("Start Video Call")}
+                    >
+                      {item.videoChat}
+                    </span>
+                  ) : isPastDate(item.renewalDate) ||
+                    item.videoChat === "Renew Subscription" ? (
+                    <span
+                      style={getStyle("Renew Subscription")}
+                      onClick={handleRenewClick}
+                    >
+                      Renew Subscription
+                    </span>
+                  ) : (
+                    <span style={getStyle(item.videoChat)}>
+                      {item.videoChat}
+                    </span>
+                  )}
+                </td>
+                <td>{item.billingAmount}</td>
+                <td>
+                  <span onClick={handleViewTeam}>
+                    <ViewIcon />
+                  </span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-    </div>
-    </div>
+      <div className="text-right reactpaginate ">
+        <ReactPaginate
+          previousLabel={<Image src={PervIcon} alt="PervIcon" />}
+          nextLabel={<Image src={NextIcon} alt="NextIcon" />}
+          breakLabel={"..."}
+          breakClassName={"break-me"}
+          pageCount={Math.ceil(data.length / rowsPerPage)}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageClick}
+          containerClassName={
+            "inline-flex mt-[34px] rounded-[5px] border border-[#d5dce9]"
+          }
+          pageClassName={"text-[#26395e]"} // List item
+          pageLinkClassName={"py-2 px-4 inline-block"} // Anchor tag
+          activeClassName={"bg-[#26395e] rounded-[5px] text-white"} // Active anchor
+          previousLinkClassName={"py-2 px-4 inline-block"}
+          nextLinkClassName={"py-2 px-4 inline-block"}
+          disabledClassName={"opacity-50 cursor-not-allowed"}
+        />
+      </div>
+
+      {/* Renew Popup Component */}
+      <Modal isOpen={renewPopupOpen} onClose={renewClosePopup}>
+        <div className="popup-content">
+          <h1 className="font-antic text-[#283C63] text-[30px] leading-[1.2em] mb-[25px] lg:text-[40px] lg:mb-[50px]">
+            Renew Plan
+          </h1>
+          <div className="">
+            <h2>Select your Plan</h2>
+            <form
+              acceptCharset="UTF-8"
+              action=""
+              method="post"
+              id="payment-form"
+              className="space-y-8"
+            >
+              <div className="Plane-widget !mt-[10px]">
+                <p className="max-w-[1000px] w-full">
+                  Your previous plan is already selected. If you cannot change
+                  your plan, please ignore this message. If you can make a
+                  change, please select from the options below.
+                </p>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-[15px]">
+                  <div className="form-group card-label max-w-[430px] w-full">
+                    <select name="about_plane" className="w-full">
+                      <option value="Video , Messaging" selected>
+                        Video , Messaging
+                      </option>
+                      <option value="Messaging">Messaging</option>
+                      <option value="Video , Messaging , Workshops">
+                        Video , Messaging , Workshops
+                      </option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+
+              <div className="payment-widget max-w-[840px] w-full">
+                <h3>Payment Method</h3>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+                  <div className="form-group card-label">
+                    <input
+                      type="text"
+                      id="card_name"
+                      placeholder="Name on Card"
+                      className="w-full"
+                    />
+                  </div>
+                  <div className="form-group card-label">
+                    <input
+                      type="text"
+                      id="cardNumber"
+                      className="w-full"
+                      placeholder=" Card Number"
+                      maxLength={19}
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-wrap gap-4 mt-4">
+                  <div className="form-group card-label max-w-[230px] w-full">
+                    <input
+                      type="text"
+                      id="cvc"
+                      className="w-full"
+                      placeholder="CVV"
+                      maxLength={13}
+                    />
+                  </div>
+                  <div className="form-group card-label max-w-[185px] w-full">
+                    <input
+                      type="text"
+                      id="expiration"
+                      className="w-full"
+                      placeholder="Expiry Month"
+                      maxLength={2}
+                    />
+                  </div>
+                  <div className="form-group card-label max-w-[185px] w-full">
+                    <input
+                      type="text"
+                      id="Year"
+                      className="w-ful"
+                      placeholder="Expiry Year"
+                      maxLength={4}
+                    />
+                  </div>
+                </div>
+                <div className="submit-section flex gap-[20px] items-center mt-4">
+                  <button type="submit" className="button">
+                    Confirm and Pay <ButtonArrow />
+                  </button>
+                  <button
+                    className="button !bg-transparent !text-[#283c63] border-[#283c63] border-[1px]"
+                    type="button"
+                    onClick={renewClosePopup}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </Modal>
+
+      {/* Renew Popup Component */}
+      <Modal isOpen={teamPopupOpen} onClose={handleCloseTeam}>
+        <h1 className="font-antic text-[#283C63] text-[30px] leading-[1.2em] mb-[25px] lg:text-[40px] lg:mb-[50px]">
+          Care Team
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-[20px] gap-y-[20px] lg:gap-y-[40px]">
+          {therapists.map((therapist) => (
+            <div key={therapist.id} className="">
+              <Image
+                src={therapist.imageUrl}
+                alt={therapist.name}
+                className="rounded-[20px] w-full aspect-square cover"
+              />
+              <h4 className="mt-4 font-gotham">{therapist.name}</h4> 
+            </div>
+          ))}
+        </div>
+        <button
+          className="button !bg-transparent !text-[#283c63] border-[#283c63] border-[1px]"
+          type="button"
+          onClick={handleCloseTeam}
+        >
+          Close
+        </button>
+      </Modal>
     </>
   );
 };
